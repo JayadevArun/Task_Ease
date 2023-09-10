3bb88a66b5fd184478ec9286fde31039c9da8103
JayadevArun <jayadevarun03@gmail.com>
2023-09-10 22:49:35 +0530

    mongoose connect

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/views", express.static('views')); 
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
