022111dee040b3dc3d45cf71ae2c166fbb906abc
JayadevArun <jayadevarun03@gmail.com>
2023-09-11 15:48:12 +0530

    created default items

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/views", express.static('views')); 
 mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new activity"
+});
+
+const item3 = new Item({
+  name: "<==Hit this to delete an activity"
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
