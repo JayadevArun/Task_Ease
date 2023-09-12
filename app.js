c797a6bbabd69edd8fd1b8321d9417cff9234e20
JayadevArun <jayadevarun03@gmail.com>
2023-09-12 09:27:55 +0530

    deleting items

--- app.js
+++ app.js
@@ -3,11 +3,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use("/views", express.static('views')); 
+
 mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 
 const itemSchema = {
@@ -71,9 +76,18 @@ app.post("/", function (req, res) {
   res.redirect("/");
 
 });
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("Successfully deleted checked item");
+      res.redirect("/");
     }
   });
 });
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
